c73b7fd37f257dd2d338379fba6cb3dea5d23c1a
jackie <jm20wd@gmail.com>
2024-08-12 21:54:37 +0100

    Rename mongo as entry and relocate to models folder

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const Entry = require('./mongo')
+const Entry = require('./models/entry')
 const app = express()
 
 
