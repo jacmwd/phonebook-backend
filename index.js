99075c84de0e5e7bc438251e955767a4e7c2b45f
jackie <jm20wd@gmail.com>
2024-08-13 22:59:05 +0100

    Change delete route handler to delete entries from database

--- index.js
+++ index.js
@@ -65,9 +65,9 @@ app.get('/info', (request, response)=> {
 })
 
 app.delete('/api/persons/:id', (request, response)=> {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+    Entry.findByIdAndDelete(request.params.id)
+        .then(result => response.status(200).end())
+        .catch(error => next(error))
 })
 
 app.post('/api/persons/', (request, response)=> {
@@ -106,6 +106,7 @@ app.post('/api/persons/', (request, response)=> {
     //response.json(person)
     
 })
+
 
 app.use(unknownEndpoint)
 
