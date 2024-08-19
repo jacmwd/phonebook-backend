0b617eeb1814b440f06771c639c8a44b23f06e7a
jackie <jm20wd@gmail.com>
2024-08-19 16:02:44 +0100

    Remove unneeded comments

--- models/entry.js
+++ models/entry.js
@@ -39,28 +39,4 @@ entrySchema.set('toJSON', {
     }
 })
 
-//const Entry = mongoose.model('Entry', entrySchema)
-
-/*const entry = new Entry({
-    name: process.argv[3],
-    number: process.argv[4]
-})
-
-if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
-} else if (process.argv.length === 3) {
-    Entry.find({}).then(result => {
-    result.forEach(person => {
-        console.log(`${person.name} ${person.number}`)
-    })
-    mongoose.connection.close()
-    })
-}else if (process.argv.length === 5) {
-    entry.save().then(result => {
-    console.log(`added ${entry.name} number ${entry.number} to phonebook`)
-    mongoose.connection.close()
-    })
-}*/
-
 module.exports = mongoose.model('Entry', entrySchema)
